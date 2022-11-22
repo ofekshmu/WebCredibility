4595c546af8840f930bd598bc503fe65628957ad
Ofek Shmuel <ofek.shmuel1@gmail.com>
2022-11-22 22:49:54 +0200

    Simple test to read an html file data

--- source/main.py
+++ source/main.py
@@ -1,5 +1,13 @@
 def main():
-    pass
+    from urllib.request import urlopen
+
+    # Download from URL and decode as UTF-8 text.
+    with urlopen( 'https://example.com/' ) as webpage:
+        content = webpage.read().decode()
+
+    # Save to file.
+    with open( 'output.html', 'w' ) as output:
+        output.write( content )
 
 if __name__ == "__main__":
     main()
